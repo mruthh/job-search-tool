71b9eef9eb7547721d453b651cddc6cd71abad72
Megan Honig <meganhonig@gmail.com>
2018-11-02 22:55:33 -0400

    add react

---
+++ react-client/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './App.css';
+import request from 'request';
+
+const baseUrl = 'http://localhost:8001'
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      jobs: []
+    }
+    this.fetchJobs = this.fetchJobs.bind(this);
+  }
+  fetchJobs(){
+    console.log(`fetching from ${baseUrl}/api/jobs/snag`);
+    request(`${baseUrl}/api/jobs/snag`, (err, res, body) => {
+      this.setState({ jobs: JSON.parse(body) });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Let's search some jobs, ok?
+          </p>
+          <p>
+            <button onClick={this.fetchJobs}>Snag 'em</button>
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
