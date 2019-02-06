5e62165487748d7022502539b60da1ce4b97f86e
Megan Honig <meganhonig@gmail.com>
2019-02-06 08:32:31 -0500

    modify export modal

--- react-client/src/ExportModal.js
+++ react-client/src/ExportModal.js
@@ -5,33 +5,63 @@ class ExportModal extends Component {
     super(props);
     this.state = {copied: false}
     this.copyToClipboard = this.copyToClipboard.bind(this);
-    this.copyText = React.createRef();
+    // this.copyText = React.createRef();
   }
 
   componentDidMount() {
     if (this.props.showModal) {
-
-      this.copyText.current.select();
-      this.copyText.current.focus();
+      // console.log(this.copyText);
+      // console.log(this.copyText.current);
+      // this.copyText.current.select();
+      // this.copyText.current.focus();
     }
 
   }
 
   copyToClipboard() {
-    this.copyText.current.select();
-    document.execCommand('copy');
+    // this.copyText.current.select();
+    const copyString = this.makeCopyString_plainText();
+    // const clipEvent = new ClipboardEvent('copy', copyString);
+    // // document.execCommand('copy');
+    // clipEvent.copy();
+    function setClipboardData(e){
+      e.clipboardData.setData("text/html", copyString);
+      e.preventDefault();
+    }
+    document.addEventListener("copy", setClipboardData);
+    document.execCommand("copy");
+    document.removeEventListener("copy", setClipboardData);
+
     this.setState({copied: true})
   }
 
   doNothing() {
     //getcher onchange handler here
   }
-  render() {
+
+  makeCopyString_richText(){
+    const fields = ['companyName', 'jobType', 'pay', 'location', 'postedDate', 'industries', 'requirements', 'cefConnections'];
+
+    return this.props.jobs.reduce((string, job) => {
+      if (!job.selected) return string;
+
+      let serialized = `<a href="${job.jobUrl}">${job.jobTitle}</a>\t`;
+
+      fields.forEach((field) => {
+        serialized += job[field];
+        serialized += '\t';
+      });
+      //replace final tab with a newline
+      // serialized = serialized.slice(0, -1).replace(/\n/g, ' ') + '\n';
+      return string + serialized;
+    }, '');
+  }
+  makeCopyString_plainText(){
     if (!this.props.showModal) return null;
 
     const fields = ['jobTitle', 'jobUrl', 'companyName', 'jobType', 'pay', 'location', 'postedDate', 'industries', 'requirements', 'cefConnections'];
 
-    const copyString = this.props.jobs.reduce((string, job) => {
+    return this.props.jobs.reduce((string, job) => {
       if (!job.selected) return string;
 
       let serialized = '';
@@ -43,7 +73,9 @@ class ExportModal extends Component {
       serialized = serialized.slice(0, -1).replace(/\n/g, ' ') + '\n';
       return string + serialized;
     }, '');
-
+  }
+  render() {
+    const copyString = this.makeCopyString_plainText();
     return (
       <div>
         <div className="row w-100">
@@ -64,13 +96,17 @@ class ExportModal extends Component {
           </div>
           <div className="row w-100">
             <div className="col-md-12 w-100 p-5">
-              <textarea readOnly
+              {/* <textarea readOnly */}
+              <div
+                contentEditable={!this.state.copied}
                 ref={this.copyText}
                 className={this.state.copied ? "form-control align-center text-center w-100" : "form-control align-center w-100"}
                 rows="25"
-                value={this.state.copied ? 'Copied!' : copyString}
+                // value={this.state.copied ? 'Copied!' : copyString}
               >
-              </textarea>
+              {this.state.copied ? 'Copied!' : copyString}
+              </div>
+              {/* </textarea> */}
             </div>
           </div>
         </div>
