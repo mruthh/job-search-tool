b883351892ce201220726655cf5ddfa31dbdb1f2
Megan Honig <meganhonig@gmail.com>
2019-08-04 19:36:03 -0400

    add error handling, clarify hard-coded value

--- fetch-indeed-jobs.js
+++ fetch-indeed-jobs.js
@@ -6,9 +6,9 @@ const { parseIndeedHTML, buildIndeedUrl } = require('./indeed');
 //helper function. makes requests for each job site
 function fetchIndeedJobs(params){
   // SUMMER 2019: WE ARE *ALWAYS* GOING TO FETCH 100 JOBS (2 PAGES) AT ONCE. ONLY QUESTION IS WHERE WE'RE STARTING
-
+  const numPages = 2
   const requests = [];
-  for (let i = 0; i < 2; i++) {
+  for (let i = 0; i < numPages; i++) {
     const request = rp({
       url: buildIndeedUrl(params),
       qs: {
@@ -17,6 +17,7 @@ function fetchIndeedJobs(params){
       }
     })
     .then(html => parseIndeedHTML(html))
+    .catch(e => console.log(e));
     requests.push(request);
   }
   return Promise.all(requests);
