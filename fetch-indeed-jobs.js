f24344a95adc747062077ad34f502a48078c90e2
Megan Honig <meganhonig@gmail.com>
2019-08-21 08:11:18 -0400

    only catch errors at top level of server

--- fetch-indeed-jobs.js
+++ fetch-indeed-jobs.js
@@ -8,16 +8,17 @@ function fetchIndeedJobs(params){
   // SUMMER 2019: WE ARE *ALWAYS* GOING TO FETCH 100 JOBS (2 PAGES) AT ONCE. ONLY QUESTION IS WHERE WE'RE STARTING
   const numPages = 2
   const requests = [];
+  // page 2 = 2nd and 3rd set of 50 results
+  const startIndex = params.startIndex * 2; 
   for (let i = 0; i < numPages; i++) {
     const request = rp({
       url: buildIndeedUrl(params),
       qs: {
         limit: 50, 
-        start: (params.startIndex + i) * 50,
+        start: (startIndex + i) * 50,
       }
     })
     .then(html => parseIndeedHTML(html))
-    .catch(e => console.log(e));
     requests.push(request);
   }
   return Promise.all(requests);
